48f532d84f81eda677cf9a62005247a2afde0bbc
Naresh Joshi <thinkjoshi@gmail.com>
2023-08-29 01:13:43 +0530

    Add Search Component in Socks

---
+++ src/components/StockManagement/SearchComponent/SearchComponent.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import Select from 'react-select';
+import { Icon } from 'semantic-ui-react';
+import './SearchComponent.scss';
+
+class SearchComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyValuePairs: {},
+      key: '',
+      value: '',
+      error: null,
+    };
+    this.keyOptions = ['Option1', 'Option2', 'Option3'].map(option => ({ value: option, label: option })); // predefined list of keys
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleKeyChange = (selectedOption) => {
+    this.setState({
+      key: selectedOption.value,
+    });
+  };
+
+  handleAddClick = () => {
+    const { key, value, keyValuePairs } = this.state;
+    if (key && value && !keyValuePairs[key]) {
+      this.setState({
+        keyValuePairs: { ...keyValuePairs, [key]: value },
+        key: '',
+        value: '',
+        error: null,
+      });
+    } else {
+      this.setState({ error: 'Invalid or duplicate key-value pair' });
+    }
+  };
+
+  render() {
+    const { key, value, keyValuePairs, error } = this.state;
+    return (
+      <div className="search-component">
+        <div className="cards-container">
+          {Object.entries(keyValuePairs).map(([key, value]) => (
+            <div className="card" key={key}>
+              <div><strong>Key:</strong> {key}</div>
+              <div><strong>Value:</strong> {value}</div>
+            </div>
+          ))}
+        </div>
+        <div className="input-container">
+          <Select
+            className="select-input"
+            name="key"
+            value={this.keyOptions.find(option => option.value === key)}
+            onChange={this.handleKeyChange}
+            options={this.keyOptions}
+            placeholder="Select a key"
+            isSearchable
+          />
+          <input
+            type="text"
+            name="value"
+            value={value}
+            onChange={this.handleInputChange}
+            placeholder="Value"
+          />
+          <button onClick={this.handleAddClick}>
+            <Icon name="plus" />
+          </button>
+        </div>
+        {error && <div className="error">{error}</div>}
+      </div>
+    );
+  }
+}
+
+export default SearchComponent;
--- src/components/StockManagement/StockManagement.jsx
+++ src/components/StockManagement/StockManagement.jsx
@@ -1,18 +1,27 @@
+// StockManagement.js
+
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 import GenericTable from '../table/table';
+import './StockManagement.scss'; // Import the SCSS file here
+import SearchComponent from './SearchComponent/SearchComponent';
 
 class StockManagement extends Component {
   constructor(props) {
     super(props);
     this.state = {
         data: [],
-        columns : []
+        columns : [],
+        lastUpdate: localStorage.getItem('lastUpdate') || 'Never'
      };
+     this.updateInstruments = this.updateInstruments.bind(this);
   }
+
+
 
   componentDidMount() {
     console.log("Home Componet")
-    fetch('http://127.0.0.1:8000/tmu/get_all_stocks')
+    fetch('http://127.0.0.1:8000/tmu/get_instruments?exchange=nse')
       .then(response => response.json())
       .then((data)=>{
         data= data.map(data=> data.fields)
@@ -22,12 +31,35 @@ class StockManagement extends Component {
       });
   }
 
+  updateInstruments() {
+    fetch('http://127.0.0.1:8000/tmu/update_instruments')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data)=>{
+        console.log("Instruments updated", data);
+        const now = new Date();
+        const timestamp = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
+        localStorage.setItem('lastUpdate', timestamp);
+        this.setState({ lastUpdate: timestamp });
+      })
+      .catch((error) => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
+  }
+
   render() {
-    const { data,columns } = this.state;
+    const { data,columns,lastUpdate } = this.state;
     return (
       <div>
         <h1>Stock Management</h1>
         <hr></hr>
+        <Button onClick={this.updateInstruments} primary>Update Instruments</Button>
+        <p className="update-time">Last updated: {lastUpdate}</p>
+        <SearchComponent></SearchComponent>
         <GenericTable data={data} columns={columns} />
       </div>
     );
