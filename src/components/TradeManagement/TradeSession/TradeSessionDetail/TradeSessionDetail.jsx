cb583c8ca03cabec60a546935f54b132d7722c1b
naresh_joshi01 <7ljy5q3oex5d3euw9zuxg4mcc3l5t1@bots.bitbucket.org>
2023-11-19 01:46:30 +0530

    ATS-24-Add Loader Component

--- src/components/TradeManagement/TradeSession/TradeSessionDetail/TradeSessionDetail.jsx
+++ src/components/TradeManagement/TradeSession/TradeSessionDetail/TradeSessionDetail.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import TradeSessionHeader from "./TradeSessionHeader/TradeSessionHeader";
 import TradeBar from "./TradeBar/TradeBar"; // Import the TradeBar component
 import TradeChart from "./TradeChart/TradeChart"; // Import the InstrumentChart component
+import { Loader } from "semantic-ui-react";
 import "./TradeSessionDetail.scss";
 
 class TradeSessionDetail extends Component {
@@ -9,6 +10,9 @@ class TradeSessionDetail extends Component {
     trades: [],
     historicalData: [],
     selectedTrade: {},
+    tradesLoading: true,
+    historicalDataLoading: true,
+    dataPointsLoading: true,
   };
 
   componentDidMount() {
@@ -18,16 +22,19 @@ class TradeSessionDetail extends Component {
   }
 
   fetchTrades = () => {
+    this.setState({ tradesLoading: true });
     const { sessionInfo } = this.props;
     fetch(
       `http://127.0.0.1:8000/tmu/get_all_trades_info?trade_session_id=${sessionInfo.id}`
     )
       .then((response) => response.json())
       .then((data) => this.setState({ trades: data.data }))
-      .catch((error) => console.error("Error:", error));
+      .catch((error) => console.error("Error:", error))
+      .finally(() => this.setState({ tradesLoading: false }));
   };
 
   fetchHistoricalData = (instrumentId, tradeDate) => {
+    this.setState({ historicalDataLoading: true });
     // Replace the placeholders with the actual values
     const tradeFrequency = this.props.sessionInfo["trading_frequency"];
     const numberOfCandles = 200;
@@ -42,15 +49,18 @@ class TradeSessionDetail extends Component {
         }));
         this.setState({ historicalData: [{ data: transformedData }] });
       })
-      .catch((error) => console.error("Error:", error));
+      .catch((error) => console.error("Error:", error))
+      .finally(() => this.setState({ historicalDataLoading: false }));
   };
 
   fetchUdtsRecord = (tradeId) => {
+    this.setState({ dataPointsLoading: true });
     // Replace the placeholder with the actual value
     fetch(`http://127.0.0.1:8000/tmu/get_udts_redcord?trade_id=${tradeId}`)
       .then((response) => response.json())
       .then((data) => this.setDataPoints(data.data, this.state.selectedTrade))
-      .catch((error) => console.error("Error:", error));
+      .catch((error) => console.error("Error:", error))
+      .finally(() => this.setState({ dataPointsLoading: false }));
   };
 
   handleTradeSelection = (trade) => {
@@ -70,25 +80,51 @@ class TradeSessionDetail extends Component {
 
   render() {
     const { sessionInfo } = this.props;
-    const { trades, historicalData, dataPoints } = this.state;
+    const {
+      dataPointsLoading,
+      historicalDataLoading,
+      tradesLoading,
+      trades,
+      historicalData,
+      dataPoints,
+    } = this.state;
 
     return (
       <div className="trade-session-details">
-        <TradeSessionHeader sessionInfo={sessionInfo} dataPoints={dataPoints}/>
+        <div className="header-container">
+          {dataPointsLoading ? (
+            <Loader active inline="centered" />
+          ) : (
+            <TradeSessionHeader
+              sessionInfo={sessionInfo}
+              dataPoints={dataPoints}
+            />
+          )}
+        </div>
         <div className="content">
           <div className="trade-bar-container">
-            <TradeBar
-              handleTradeSelection={this.handleTradeSelection}
-              trades={trades}
-            />
+            {tradesLoading ? (
+              <Loader active inline="centered" />
+            ) : (
+              <TradeBar
+                handleTradeSelection={this.handleTradeSelection}
+                trades={trades}
+              />
+            )}
           </div>
           <div className="instrument-chart-container">
-            {(historicalData.length && Object.keys(dataPoints).length) ? (
-              <TradeChart
-                historicalData={historicalData}
-                dataPoints={dataPoints}
-              />
-            ) : null}
+            {historicalData.length && Object.keys(dataPoints).length ? (
+              historicalDataLoading || dataPointsLoading ? (
+                <Loader active inline="centered" />
+              ) : (
+                <TradeChart
+                  historicalData={historicalData}
+                  dataPoints={dataPoints}
+                />
+              )
+            ) : (
+              <div>Please Select A Trade to see the chart</div>
+            )}
           </div>
         </div>
       </div>
